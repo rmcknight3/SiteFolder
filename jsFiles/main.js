0692edad7bcc7c6942702029b0f72163fd6c5682
Richard <rmcknight3@gmail.com>
2015-05-12 19:10:28 -0400

    Updated

--- jsFiles/main.js
+++ jsFiles/main.js
@@ -3,12 +3,12 @@
   var $image = $("<img>");
   var $caption = $("<p class='imgCaption'></p>");
 
-
 //Adds (appends) $overlay variable to the document with the '$image' var that will be placed within the <div> tags
 $overlay.append($image);
 
 //Add overlay to body of document
   $("body").append($overlay);
+//Puts caption under imgage
   $overlay.append($caption);
 
 //Capture the click event on a link to an image
@@ -21,16 +21,15 @@ $("#imageGallery a").click(function(event){
 
   //Show the overlay
   $overlay.show();
-  //code here
 
   //Get child alt attribute for the caption text
   var captionText = $(this).children("img").attr("alt");
   $caption.text(captionText);
 });
 
-//When overlay is clicked
-  //hide overlay
-$overlay.click(function(){
+//When overlay is clicked...
+  $overlay.click(function(){
+    //...hide overlay
   $(this).hide();
 });
 
@@ -38,31 +37,19 @@ $overlay.click(function(){
 $(document).keyup(function(e){
 	if (e.which == 27){
 		$("#overlay").hide();
-
 	}
 	console.log(e);
-
 });
 
 // '.hover()' to change background color of navigation links on mouse hover
 $(".navigation a").hover(
 	function(){
 		//change to this color when mouse enters element
-      // $(this).css("border", "solid #000000 1px")
       $(this).css("background-color", "#8bafFF");
       $(this).css("color", "#000000");
     }, 
     function(){
     	//change back when mouse leaves element
-      // $(this).css("border", "none")
       $(this).css("background-color", "#FFc34e");
       $(this).css("color", "#000000");
 });
-
-
-
-
-
-
-
-
